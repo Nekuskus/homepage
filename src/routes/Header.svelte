729c1b90281e63226d536f32dc4c447c9b398d54
Nekuskus <61391965+Nekuskus@users.noreply.github.com>
2024-06-19 19:14:20 +0200

    Replace tabs with spaces

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,13 +1,13 @@
 <script>
-	// import { page } from '$app/stores';
-	import kuskus_logo from "$lib/images/kuskus-cropped.jpg";
+    // import { page } from '$app/stores';
+    import kuskus_logo from "$lib/images/kuskus-cropped.jpg";
 </script>
 
 <header>
-	<h1>
-		<span id='at'>@</span><span id="https">https://</span>kuskus.dev
-	</h1>
-	<!-- TODO: Add couscous spelling and pronunciation variations -->
+    <h1>
+        <span id='at'>@</span><span id="https">@</span>kuskus.dev
+    </h1>
+    <!-- TODO: Add couscous spelling and pronunciation variations -->
 </header>
 
 <div id="avatar-wrapper">
@@ -19,40 +19,40 @@
 </div>
 
 <style>
-	header {
-		/* display: flex; */
+    header {
+        /* display: flex; */
 
-		padding-left: var(--outside-padding);
-		padding-top: var(--outside-padding);
-		padding-right: var(--outside-padding);
-		padding-bottom: 1em;
-	}
+        padding-left: var(--outside-padding);
+        padding-top: var(--outside-padding);
+        padding-right: var(--outside-padding);
+        padding-bottom: 1em;
+    }
 
     h1 {
         font-weight: bold;
         text-align: left;
     }
     
-	h1 span#at {
-		display: inline-block;
-		margin-right: 0.25em;
-		font-size: 1.25em;
+    h1 span#at {
+        display: inline-block;
+        margin-right: 0.25em;
+        font-size: 1.25em;
 
-		color: var(--color-theme-1);
-	}
+        color: var(--color-theme-1);
+    }
 
-	h1 span#https {
-		display: none;
-		position: relative;
+    h1 span#https {
+        display: none;
+        position: relative;
 
-		left: -1em;
-		margin-right: 0.25em;
-		font-size: 1.25em;
+        left: -1em;
+        margin-right: 0.25em;
+        font-size: 1.25em;
 
-		color: var(--color-theme-1);
-		opacity: 0;
-	}
-	
+        color: var(--color-theme-1);
+        opacity: 0;
+    }
+    
     @media (min-width: 600px) {
         h1:hover span#https {
             display: inline-block;
@@ -67,56 +67,56 @@
             
             animation: slide-out 0.5s forwards;
         }
-	}
+    }
 
-	
-	@keyframes slide-in {
-		0% {
-			opacity: 0;
-			top: -1em;
-		}
-		100% {
-			opacity: 1;
-			top: 0;
-		}
-	}
-	
-	@keyframes slide-out {
-		0% {
-			opacity: 1;
-			top: 0;
-		}
-		100% {
-			opacity: 0;
-			top: 1em;
-		}
-	}
+    
+    @keyframes slide-in {
+        0% {
+            opacity: 0;
+            top: -1em;
+        }
+        100% {
+            opacity: 1;
+            top: 0;
+        }
+    }
+    
+    @keyframes slide-out {
+        0% {
+            opacity: 1;
+            top: 0;
+        }
+        100% {
+            opacity: 0;
+            top: 1em;
+        }
+    }
 
-	div#avatar-wrapper {
-		position: absolute;
-		top: var(--outside-padding);
-		right: var(--outside-padding);
-	}
+    div#avatar-wrapper {
+        position: absolute;
+        top: var(--outside-padding);
+        right: var(--outside-padding);
+    }
 
-	img#avatar-logo {
-		width: 24vw;
-		border-radius: 100%;
-	}
-	
-	@media (max-width: 1200px) {
-		div#avatar-wrapper {
-			top: calc(var(--outside-padding) / 2);
-			right: calc(var(--outside-padding) / 2);
-		}
+    img#avatar-logo {
+        width: 24vw;
+        border-radius: 100%;
+    }
+    
+    @media (max-width: 1200px) {
+        div#avatar-wrapper {
+            top: calc(var(--outside-padding) / 2);
+            right: calc(var(--outside-padding) / 2);
+        }
 
-		img#avatar-logo {
-			width: 16.56vw;
-		}
-	}
+        img#avatar-logo {
+            width: 16.56vw;
+        }
+    }
 
-	@media (max-width: 1600px) {
-		img#avatar-logo {
-			width: 12vw;
-		}
-	}
+    @media (max-width: 1600px) {
+        img#avatar-logo {
+            width: 12vw;
+        }
+    }
 </style>
