cd633b0c8051cedd3c70988b35ccc8da96174861
Nekuskus <61391965+Nekuskus@users.noreply.github.com>
2024-06-19 17:44:34 +0200

    Basic header animation

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -5,7 +5,7 @@
 
 <header>
 	<h1>
-		<span title="https://">@</span>kuskus.dev
+		<span id='at'>@</span><span id="https">https://</span>kuskus.dev
 	</h1>
 	<!-- TODO: Add couscous spelling and pronunciation variations -->
 
@@ -33,11 +33,62 @@
 		}
 	}
 
-	h1::first-letter {
+	h1 span#at {
+		display: inline-block;
 		margin-right: 0.25em;
 		font-size: 1.25em;
 
 		color: var(--color-theme-1);
+	}
+
+	h1 span#https {
+		display: none;
+		position: relative;
+
+        left: -1em;
+		margin-right: 0.25em;
+		font-size: 1.25em;
+
+		color: var(--color-theme-1);
+		opacity: 0;
+	}
+	
+
+	h1:hover span#https {
+		display: inline-block;
+		position: relative;
+		
+		animation: slide-in 0.5s forwards;
+	}
+	
+	h1:hover span#at {
+		display: inline-block;
+		position: relative;
+		
+		animation: slide-out 0.5s forwards;
+	}
+
+	
+	@keyframes slide-in {
+		0% {
+			opacity: 0;
+			top: -1em;
+		}
+		100% {
+			opacity: 1;
+			top: 0;
+		}
+	}
+	
+	@keyframes slide-out {
+		0% {
+			opacity: 1;
+			top: 0;
+		}
+		100% {
+			opacity: 0;
+			top: 1em;
+		}
 	}
 
 	div#avatar-wrapper {
