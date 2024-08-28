68e0dd047503db84ee7a2ef53aa7b60a7a6a8297
Nekuskus <contact@kuskus.dev>
2024-08-28 18:40:45 +0200

    v0.9.1 - Fix prerender deployment

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,4 +1,4 @@
-<script lang="ts">
+<script context="module" lang="ts">
     import Comment from '../Comment.svelte';
 
     import { marked } from "marked";
--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,35 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import fs from "fs";
+
+async function getEntries() {
+
+    let entries = fs.readdirSync("src/lib/contents/blog")
+        .filter(name => name.endsWith('.md'))
+        .map(name => name.replace('.md', '').split(' '))
+        .map(split => `/blog/${split[0]}/${split[1]}`);
+
+    entries.push('*');
+
+    return entries;
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
 
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
-	}
+    kit: {
+        adapter: adapter({
+            pages: 'build',
+            assets: 'build',
+            fallback: undefined,
+            precompress: false,
+            strict: true
+        }),
+        prerender: {
+            entries: await getEntries()
+        }
+    }
 };
 
 export default config;
